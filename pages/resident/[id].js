86bdf0c5c6c06096ab8711d2eb7be8210f33accf
EmilianoBelvedere <emiliano@raisedreal.com>
2021-07-03 13:31:05 -0300

    Remove loader fallback

--- pages/resident/[id].js
+++ pages/resident/[id].js
@@ -1,47 +1,40 @@
 import Link from 'next/link';
 import { Button, Avatar, Stack, GridItem, Grid, Divider } from "@chakra-ui/react";
-import { useRouter } from "next/router"
 
 import { initializeApollo } from '../../lib/apolloClient';
 import { GET_RESIDENT } from '../../lib/queries'
 import LabelField from '../../components/generics/labelField'
 
-const ResidentPage = ({ resident }) => {
-  const router = useRouter()
+const ResidentPage = ({ resident }) => (
+  <Stack direction="column">
+    <Stack direction="row" spacing={4} p={8} align="center" justify="center">
+      <Link href={`/`}>
+        <a><Button colorScheme="teal" variant="link">Search Locations</Button></a>
+      </Link>
 
-  if (router.isFallback) return <h1>Loading...</h1>
-
-  return (
-    <Stack direction="column">
-      <Stack direction="row" spacing={4} p={8} align="center" justify="center">
-        <Link href={`/`}>
-          <a><Button colorScheme="teal" variant="link">Search Locations</Button></a>
-        </Link>
-
-        <Link href={`/location/[id]`} as={`/location/${resident.location.id}`}>
-          <a><Button colorScheme="teal" variant="link">{resident.location.name}</Button></a>
-        </Link>
-      </Stack>
-      
-      <Divider mb={4}/>
-
-      <Grid
-        templateColumns="repeat(6, 1fr)"
-        gap={8}
-      >
-        <GridItem colSpan={2} >
-          <Avatar size="4xl" name={resident.name} src={resident.image} />
-        </GridItem>
-        <GridItem colSpan={4} mt={4}>
-          <LabelField label={'Name:'} value={resident.name} />
-          <LabelField label={'Gender:'} value={resident.gender} />
-          <LabelField label={'Status:'} value={resident.status} />
-          <LabelField label={'Species:'} value={resident.species} />
-        </GridItem>
-      </Grid>
+      <Link href={`/location/[id]`} as={`/location/${resident.location.id}`}>
+        <a><Button colorScheme="teal" variant="link">{resident.location.name}</Button></a>
+      </Link>
     </Stack>
-  )
-}
+    
+    <Divider mb={4}/>
+
+    <Grid
+      templateColumns="repeat(6, 1fr)"
+      gap={8}
+    >
+      <GridItem colSpan={2} >
+        <Avatar size="4xl" name={resident.name} src={resident.image} />
+      </GridItem>
+      <GridItem colSpan={4} mt={4}>
+        <LabelField label={'Name:'} value={resident.name} />
+        <LabelField label={'Gender:'} value={resident.gender} />
+        <LabelField label={'Status:'} value={resident.status} />
+        <LabelField label={'Species:'} value={resident.species} />
+      </GridItem>
+    </Grid>
+  </Stack>
+)
 
 export async function getServerSideProps({ params }) {
   const queryId = params.id;
